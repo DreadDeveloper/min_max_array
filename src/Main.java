8e71bb0b35dbe0cc6a745ab9420be9c0d1d3aa0d
Abhinav Kumar <kumaraka2000@gmail.com>
2023-04-14 14:28:26 +0530

    Initial Commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Scanner;
+public class Main
+{
+    public static void main(String[] args)
+    {
+        List<Integer> arr = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the length of the array: ");
+        int length = scanner.nextInt();
+        for(int i = 0; i < length; i++)
+        {
+            System.out.print("Enter the value: ");
+            arr.add(scanner.nextInt());
+        }
+        arr.sort(Comparator.naturalOrder());
+        System.out.println("The minimum integer is: " + arr.get(0) +
+                "\nThe maximum integer is: " + arr.get(arr.size() - 1));
     }
 }
